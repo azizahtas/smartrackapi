02bd909df02ea8d38611f7315480267e7c38b0ea
Aziz Ahtas <azizahtas@gmail.com>
2018-05-04 20:51:06 +0530

    Fixed Integer Percent full issue

--- routes/levels.js
+++ routes/levels.js
@@ -10,11 +10,11 @@ router
       var start = data.startDate;
       var end = data.endDate;
 
-      var querry = "SELECT rack_num,shelf_num,percent_full*100 AS percent,date_recorded,url FROM shelf_levels " + 
+      var querry = "SELECT rack_num,shelf_num,percent_full::integer*100 AS percent,date_recorded,url FROM shelf_levels " + 
               "WHERE date_recorded > '"+start+"' AND  date_recorded < '"+end+"' " + 
               "AND rack_num = '"+racknum+"' " + 
               "ORDER BY shelf_levels.date_recorded ASC";
-
+      console.log(querry);
       var client = new Client({
           user: 'azizahtas',
           host: 'smart-rack.c7neg6roarnk.us-east-1.rds.amazonaws.com',
