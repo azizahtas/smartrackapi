079d98fddf1c752c4e56b3aa964b668136c4ce1a
Aziz Ahtas <azizahtas@gmail.com>
2018-05-04 16:53:34 +0530

    added CORS

--- app.js
+++ app.js
@@ -29,6 +29,14 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
