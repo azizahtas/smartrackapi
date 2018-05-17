c5c326332932078794799319719dae10112a9278
azizahtas <azizahtas@gmail.com>
2018-05-17 16:15:57 +0530

    Added Mqtt Server

--- app.js
+++ app.js
@@ -7,8 +7,28 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var levelsRouter = require('./routes/levels');
-
+var mosca = require('mosca');
 var app = express();
+var ascoltatore = {
+  type: 'redis',
+  redis: require('redis'),
+  port: 15356,
+  return_buffers: true, // to handle binary payloads
+  host: "redis-15356.c11.us-east-1-2.ec2.cloud.redislabs.com",
+  password: "kbDG1s4EyMaVbQ1Y7bCErLE2lQ0ciyvd"
+};
+var moscaSettings = {
+  port: 1883,
+  backend: ascoltatore,
+  persistence: {
+    factory: mosca.persistence.Redis,
+    host:  "redis-15356.c11.us-east-1-2.ec2.cloud.redislabs.com",
+    port: 15356,
+    password: "kbDG1s4EyMaVbQ1Y7bCErLE2lQ0ciyvd"
+  }
+};
+var server = new mosca.Server(moscaSettings);
+server.on('ready', setup);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -48,4 +68,17 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+server.on('clientConnected', function(client) {
+	console.log('client connected', client.id);		
+});
+
+// fired when a message is received
+server.on('published', function(packet, client) {
+  console.log('Published', packet.topic, packet.payload);
+});
+
+// fired when the mqtt server is ready
+function setup() {
+  console.log('Mosca server is up and running')
+}
 module.exports = app;
