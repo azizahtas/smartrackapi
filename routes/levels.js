31f17141852c2942cbed8697b7d8e8e615512d47
azizahtas <azizahtas@gmail.com>
2018-06-13 17:21:16 +0530

    Added local_time

--- routes/levels.js
+++ routes/levels.js
@@ -11,10 +11,10 @@ router
       var start = data.startDate;
       var end = data.endDate;
 
-      var querry = "SELECT racknum,shelf_num,percent_full*100 AS percent,date_recorded,url FROM shelf_stock " +
-              "WHERE date_recorded > '"+start+"' AND  date_recorded < '"+end+"' " +
+      var querry = "SELECT racknum,shelf_num,percent_full*100 AS percent,date_recorded,local_time,url FROM shelf_stock " +
+              "WHERE local_time > '" + start + "' AND  local_time < '" + end + "' " +
               "AND racknum = '"+racknum+"' " +
-              "ORDER BY shelf_stock.date_recorded ASC";
+              "ORDER BY local_time ASC";
       console.log(querry);
       var client = new Client(settings.database.postgres);
         client.connect();
